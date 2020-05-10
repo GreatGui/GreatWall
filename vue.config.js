a8187c931e0a79760ff9156357e5e040be2af34d
GreatGui <gui_jorgelima@hotmail.com>
2020-05-10 15:19:32 -0300

    GreatWall v0.1.3

---
+++ vue.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      outputDir: "dist_electron",
+      builderOptions: {
+        productName: "GreatWall",
+        artifactName: "GreatWall-${version}.${ext}",
+        copyright: "Copyright © 2020 GreatGui",
+        publish: [
+          {
+            provider: "github",
+            owner: "GreatGui",
+            repo: "GreatWall"
+          }
+        ],
+        // icon: "./public/Pictures.ico",
+        win: {
+          verifyUpdateCodeSignature: false,
+          target: [
+            {
+              target: "nsis",
+              arch: "x64"
+            }
+          ]
+        },
+        nsis: {
+          installerIcon: "./public/Pictures.ico",
+          displayLanguageSelector: true,
+          oneClick: false,
+          perMachine: false,
+          allowElevation: true,
+          allowToChangeInstallationDirectory: true
+        },
+        files: [
+          "!{dist,.gitignore,.vscode,docs,dev-app-update.yml,babel.config.js}"
+        ],
+        compression: "maximum"
+      }
+    }
+  }
+};
